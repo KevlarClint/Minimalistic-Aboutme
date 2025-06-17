73d1b9ad51316087c1b66bf6e60a074f26977479
Clint Lorenzo <clintlorenzo11@gmail.com>
2025-06-17 12:19:29 +0800

    okd

--- gsap.js
+++ gsap.js
@@ -1,4 +1,14 @@
- document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrollTrigger,SplitText,TextPlugin)
-  // gsap code here!
- });
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger, SplitText, ScrollSmoother);
+  gsap.from(".sidebar", {
+    duration: 1,
+    x: "-200px",
+    ease: "power1.inOut"
+  });
+
+  gsap.from(".box-link", {
+    duration: 1,
+    x: "-200px",
+    ease: "power1.inOut"
+  });
+});
