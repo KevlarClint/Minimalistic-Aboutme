0c7e81f9ce7cb9474cc5fefaa57d6cbb08e494e1
Clint Lorenzo <clintlorenzo11@gmail.com>
2025-06-20 13:09:15 +0800

    gsap
    gsap animation daw

--- gsap.js
+++ gsap.js
@@ -6,9 +6,28 @@ document.addEventListener("DOMContentLoaded", (event) => {
     ease: "power1.inOut"
   });
 
-  gsap.from(".box-link", {
-    duration: 1,
-    x: "-200px",
-    ease: "power1.inOut"
-  });
+  const linkPopUp = gsap.timeline({defaults: {opacity: 0}});
+  linkPopUp.fromTo(
+    ".box-link1",
+    {duration: 0.2},
+    {opacity: 1, scale: 1, delay: 0} // starts at t = 3s
+  );
+  linkPopUp.fromTo(
+    ".box-link2",
+    {duration: 0.2},
+    {opacity: 1, scale: 1, delay: 0.1} // starts at t = 3s
+  );
+  linkPopUp.fromTo(
+    ".box-link3",
+    {duration: 0.2},
+    {opacity: 1, scale: 1, delay: 0.2} // starts at t = 3s
+    
+  );
+
+
+  // gsap.fromTo(
+  //   ".box-link",
+  //   {opacity: 0, duration: 1, x: -200},
+  //   {opacity: 1, x: 0} 
+  // );
 });
